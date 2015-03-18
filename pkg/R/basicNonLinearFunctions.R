6bbb00524c2b50ff441d1702977c33d8870a69c7
gaussllego <gaussllego@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2015-03-18 19:04:10 +0000

    Mutual information functionality
    git-svn-id: svn://r-forge.r-project.org/svnroot/nonlinear@30 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/R/basicNonLinearFunctions.R
+++ pkg/R/basicNonLinearFunctions.R
@@ -53,7 +53,7 @@ isNeighbour=function(v1,v2,embedding.dim,radius){
 #' @references H. Kantz  and T. Schreiber: Nonlinear Time series Analysis (Cambridge university press)
 #' @author Constantino A. Garcia
 #' @examples 
-#'\dontrun{
+#' \dontrun{
 #'# Build the Takens vector for the Henon map using the x-coordinate time series
 #' h = henon(n.sample=  3000,n.transient= 100, a = 1.4, b = 0.3, 
 #' start = c(0.73954883, 0.04772637), do.plot = FALSE)
@@ -85,100 +85,139 @@ buildTakens=function (time.series, embedding.dim, time.lag)
 #' time lag (timeLag), the \eqn{n^{th}} 
 #' Takens' vector is defined as 
 #' \deqn{T[n]=\{time.series[n], time.series[n+ timeLag],...time.series[n+m*timeLag]\}.}
-#' This function estimates an appropiate time lag by using the autocorrelation function.
+#' This function estimates an appropiate time lag by using the autocorrelation function
+#' or the average mutual information .
 #' @details 
 #' A basic criteria for estimating a proper time lag is based on the following reasoning:
 #' if the time lag used to build the Takens' vectors is too small, the coordinates will
 #' be too highly temporally correlated and the embedding will tend to cluster around 
-#' the diagonal in the phase space. If the time lag is chosen too large, the resulting coordinates may be almost uncorrelated
-#' and the resulting embedding will be very complicated.  Thus, there is a wide variety of methods
-#' for estimating an appropiate time lag  based on the study of the autocorrelation function
-#' of a given time series:
-#'  \itemize{
-#'    \item Select the time lag where the autocorrelation function decays to 0 (first.zero method).
-#'    \item Select the time lag where the autocorrelation function decays to 1/e (first.e.decay method).
-#'    \item Select the time lag where the autocorrelation function reaches its first minimum (first.minimum method).
-#'    \item Select the time lag where the autocorrelation function decays to the value specified by the user (first.value method and value parameter).
+#' the diagonal in the phase space. If the time lag is chosen too large, the resulting 
+#' coordinates may be almost uncorrelated and the resulting embedding will be very complicated. 
+#' Thus, the autocorrelation function can be used for  estimating an appropiate time lag of
+#' a time series. However, it must be noted that the autocorrelation is a linear statistic,
+#' and thus it does not take into account nonlinear dynamical correlations. To take into
+#' account nonlinear correlations the average mutual information (AMI) can be used. 
+#' Independently of the technique used to compute the correlation, the time lag can
+#'  be selected in a variety of ways:   
+#' \itemize{
+#'    \item Select the time lag where the autocorrelation/AMI function decays to 0 
+#'    (\emph{first.zero} selection method). This
+#'    method is not appropriate for the AMI function, since it only takes positive values.
+#'    \item Select the time lag where the autocorrelation/AMI function decays to 
+#'    1/e of its value at zero (\emph{first.e.decay} selection method).
+#'    \item Select the time lag where the autocorrelation/AMI function reaches 
+#'    its first minimum (\emph{first.minimum} selection method).
+#'    \item Select the time lag where the autocorrelation/AMI function decays to
+#'     the value specified by the user (\emph{first.value} selection method and 
+#'     \emph{value} parameter).
 #' }   
 #' @param time.series The original time series.
-#' @param method The method that we shall use to estimate the time lag (see the Details section). Available methods
-#' are \emph{"first.zero"}, \emph{"first.e.decay"} (default), \emph{"first.minimum"} and \emph{"first.value"}. 
-#' @param value Numeric value indicating the value that the autocorrelation function must cross in order to
-#' select the time lag. It is used only with the "first.value" method.
-#' @param lag.max Maximum lag at which to calculate the acf. By default, the length of the time.series
-#' is used.
-#' @param do.plot Logical value. If TRUE (default value), a plot of the autocorrelation function is shown.
+#' @param technique The technique that we shall use to estimate the time lag (see the 
+#' Details section). Allowed values are \emph{"acf"} and \emph{"ami"}.
+#' @param selection.method Method used for selecting a concrete time lag. 
+#' Available methods are \emph{"first.zero"}, \emph{"first.e.decay"} (default),
+#'  \emph{"first.minimum"} and \emph{"first.value"}. 
+#' @param value Numeric value indicating the value that the autocorrelation/AMI 
+#' function must cross in order to select the time lag. It is used only with
+#'  the "first.value" selection method.
+#' @param lag.max Maximum lag at which to calculate the acf/AMI. 
+#' @param do.plot Logical value. If TRUE (default value), a plot of the 
+#' autocorrelation/AMI function is shown.
+#' @param main A title for the plot.
+#' @param ... Additional parameters for the \emph{acf} or the 
+#' \emph{mutualInformation}.
 #' @return The estimated time lag.
-#' @note If the autocorrelation function does not cross the specifiged value, an error is thrown. This may be solved
-#' by increasing the lag.max or selecting a higher value to which the autocorrelation function must decay.
+#' @examples
+#' \dontrun{
+#' sx = sinaiMap(a=0.3,n.sample=5000,start=c(0.23489,0.8923),do.plot=FALSE)$x
+#' timeLag(sx, technique="ami",  
+#'         n.partitions = 20, units = "Bits")
+#' timeLag(sx, technique="acf") }
+#' @note If the autocorrelation/AMI function does not cross the specifiged value, 
+#' an error is thrown. This may be solved by increasing the \emph{lag.max} or 
+#' selecting a higher value to which the autocorrelation/AMI function may decay.
+#' @seealso \code{\link{mutualInformation}}
 #' @references H. Kantz  and T. Schreiber: Nonlinear Time series Analysis (Cambridge university press)
 #' @author Constantino A. Garcia
 #' @export timeLag
-timeLag = function (time.series, method="first.e.decay",value = 1/exp(1),lag.max=NULL,do.plot=TRUE){
-  ##############################################################################
-  ########################### internal functions definitions ###################
-  #############################################################################
-  # internal function. It will calculate the first time lag where
-  # the autocorrelation function decays to a given value
-  position.acf.decays=function(time.series,value,lag.max=NULL,do.plot){
-      if (is.null(lag.max)){
-        l.time.series = length(time.series)
-        lag.max = floor(l.time.series/2)
-      }
-      ac = stats::acf(time.series,lag.max=lag.max,plot=do.plot,type="correlation")
-      ac = ac$acf
-      cross.position <- which(ac < value)
-      # if the autocorrelation function does not cross the value specified
-      # by the user, a warning is given
-      if (length(cross.position) == 0){
-        stop(paste("The autocorrelation function does not cross ", value,
-                   ". Choose another \"cross\" value!\n",sep=""))
-      }else{   
-        cross.position = cross.position[[1]]
-        # select closest point to given value
-        if ((cross.position > 1) && (abs(ac[[cross.position]] - value) > abs(ac[[cross.position - 1]] - value)))
-          cross.position = cross.position - 1
-      }
-      # If possible: convert from positions to time lags by substracting 1
-      if ((cross.position) > 1){cross.position = cross.position-1}
-      return (cross.position)
-  }
+timeLag = function (time.series, technique=c("acf","ami"), 
+                    selection.method=c("first.e.decay", "first.zero", 
+                                       "first.minimum", "first.value"),
+                    value = 1/exp(1), lag.max=NULL, do.plot=TRUE,
+                    main=NULL,...){
   
-  # internal function. It will calculate the first time lag where
-  # the autocorrelation function has a minimum
-  first.acf.minimum=function(time.series,lag.max=NULL,do.plot){
-    if (is.null(lag.max)){
-      l.time.series = length(time.series)
-      lag.max = l.time.series
+  technique = match.arg(technique)
+  if (is.null(main)){
+    main = switch(technique,
+                  acf = "Autocorrelation function",
+                  ami = "Average Mutual Information (AMI)")
+  }
+  if( is.null(lag.max) && (method == "acf") ){
+    
+    lag.max = max(20, length(time.series)/2)
+    # if method=="ami", the mutualInformation function will handle the null value
+  }
+  fx = switch(technique, 
+              ami = mutualInformation(time.series,lag.max = lag.max,
+                                      do.plot=do.plot,main=main,... )$mutual.information,
+              acf = stats::acf(time.series,lag.max=lag.max,plot=do.plot,
+                               type="correlation",main=main,...)$acf
+              )
+  
+  
+  selection.method = match.arg(selection.method)
+  lag = switch(match.arg(selection.method),
+               first.e.decay = get.position.decays(fx,fx[1]/exp(1),technique),
+               first.zero = get.position.decays(fx,0,technique),
+               first.minimum = get.first.minimum(fx,technique),
+               first.value = get.position.decays(fx,value,technique) )
+  if (do.plot){
+    if (selection.method == "first.e.decay"){
+      abline(h=fx[1]/exp(1),lty=3,col=3,lwd=2)
+    }else if(selection.method == "first.value"){
+      abline(h=value,lty=3,col=3,lwd=2)
     }
-    ac = stats::acf(time.series,lag.max=lag.max,plot=do.plot,type="correlation")
-    ac = ac$acf
-    first.minimum = which(diff(ac) >= 0)
-    # if the autocorrelation function is monotonically decreasing, an error is given
-    if (length(first.minimum) == 0) {
-      stop(paste("The autocorrelation function does not have a minimum\n",sep=""))
-    }else{
-      first.minimum = first.minimum[[1]]
-    } 
-    # convert from positions to time lags by substracting 1
-    return (first.minimum-1)
-  }
-  #############################################################################
-  #############################################################################
-  time.lag=0
-  switch(method,
-         "first.zero"={time.lag=position.acf.decays(time.series,0,lag.max,do.plot=do.plot)},
-         "first.e.decay"={time.lag=position.acf.decays(time.series,1/exp(1),lag.max,do.plot=do.plot)},
-         "first.value"={
-                        if ((value < 0)||(value>1))
-                          stop(paste("Incorrect \"cross\" value", value,
-                                      ". \"Cross\" value must be between 0 and 1.
-                                     Choose another \"cross\" value!\n",sep=""))
-                        time.lag=position.acf.decays(time.series,value,lag.max,do.plot=do.plot)},
-         "first.minimum"={time.lag=first.acf.minimum(time.series,lag.max,do.plot=do.plot)},
-         {stop("Incorrect method. Available methods are:\"fist.zero\",
-               \"first.e.decay\", \"fist.value\" and \"fist.minimum\".\n")})
-  return (time.lag)
+  } 
   
+  lag
 }
 
+
+# internal function. It will calculate the first time lag where
+# the vector x decays to a given value
+get.position.decays=function(x, value, method ){
+  cross.position <- which(x <= value)
+  # if x does not cross the value specified
+  # by the user, a warning is given
+  if (length(cross.position) == 0){
+    stop(paste("The ",method," function does not cross ", value,
+               ". Choose another \"cross\" value!\n",sep=""))
+  }else{   
+    cross.position = cross.position[[1]]
+  }
+  # If possible: convert from positions to time lags by substracting 1
+  if ((cross.position) > 1){
+    cross.position = cross.position-1
+  }
+  return (cross.position)
+}
+
+# internal function. It will calculate the first time lag where
+# the x vector has a minimum
+get.first.minimum=function(x,method){
+  derivative = diff(x)
+  derivative = ifelse( derivative < 0, -1, derivative)
+  derivative = ifelse( derivative == 0, 0, derivative)
+  derivative = ifelse( derivative > 0, 1, derivative)
+  minimums = which(diff(derivative) >= 1) 
+  # if the autocorrelation function is monotonically decreasing, an error is given
+  if (length(minimums) == 0) {
+    stop(paste("The ",method," function does not have a minimum\n",sep=""))
+  }else{
+    #take into account that we have loss some samples after diff
+    
+    first.minimum = minimums[[1]] + 1
+  } 
+  # convert from positions to time lags by substracting 1
+  return (first.minimum-1)
+}
